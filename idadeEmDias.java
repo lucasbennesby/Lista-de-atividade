f07eefbff5ae842dd30b13cefafd90fc5cf8813f
lucasbennesby <lucasbenesby@gmail.com>
2022-08-29 15:09:18 -0300

    Reformulado o codigo de idade em dias

--- idadeEmDias.java
+++ idadeEmDias.java
@@ -10,21 +10,33 @@ public class idadeEmDias {
 
         System.out.println("Digite sua idade em dias: ");
         idadeDias = leitor.nextInt();
+        // O(1)
         ano = 0;
-        while (idadeDias > 365) {
-
-            idadeDias = idadeDias - 365;
-            ano++;
-
-        }
         mes = 0;
-        while (idadeDias > 30) {
-            idadeDias = idadeDias - 30;
-            mes++;
+        dia = 0;
 
-        }
+        ano = (idadeDias / 365);
+        mes = (idadeDias % 365) / 30;
+        dia = (idadeDias % 365) % 30;
 
-        dia = idadeDias;
+        // O(N)
+        /*
+         * while (idadeDias > 365) {
+         * 
+         * idadeDias = idadeDias - 365;
+         * ano++;
+         * 
+         * }
+         * mes = 0;
+         * while (idadeDias > 30) {
+         * idadeDias = idadeDias - 30;
+         * mes++;
+         * 
+         * }
+         * 
+         * dia = idadeDias;
+         */
+
         System.out.println(ano + " ano (s)");
         System.out.println(mes + " mes (es)");
         System.out.println(dia + " dia (s)");
